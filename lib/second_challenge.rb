8375320c154d38e0105fc1973533ca4ffdb2525b
Steven Prindle <flankbusiness@gmail.com>
2020-02-23 13:11:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,15 +3,19 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {},
-      :matriarch => {},
+      :matriarch => {}, 
       :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
+      }}
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }}
-end
+      }
+    }
+
+
+end 
